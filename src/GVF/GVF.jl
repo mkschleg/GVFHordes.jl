751aefa95a23c69c4a49b5529a14b94e889122fe
AJ <a.john.jacobsen@gmail.com>
2019-03-08 15:12:19 -0700

    restructure a bit

--- src/GVF/GVF.jl
+++ src/GVF/GVF.jl
@@ -1,10 +1,9 @@
-using .Cumulants
-using .Policies
-using .Discounts
+include("Discounts.jl")
+include("Cumulants.jl")
+include("Policies.jl")
 
 struct GVF{C,D,π}
     cumulant::C
     discount::D
     policy::π
 end
-
--- src/Horde.jl
+++ src/Horde.jl
@@ -1,9 +1,5 @@
 module Horde
 
-include("GVF/Cumulants.jl")
-include("GVF/Discounts.jl")
-include("GVF/Policies.jl")
 include("GVF/GVF.jl")
-
 
 end # module
